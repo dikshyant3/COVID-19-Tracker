eea1711074aae2ebde6254001df2c48eda09df73
dikshyant <dhunganadikshyant12@gmail.com>
2025-03-11 20:17:08 +0545

    updated all the dependencies and code

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -1,56 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 import numeral from "numeral";
-// import {
-//   Chart as ChartJS,
-//   LineElement,
-//   PointElement,
-//   LinearScale,
-//   Title,
-// } from "chart.js";
+import {
+  Chart as ChartJS,
+  LineElement,
+  PointElement,
+  LinearScale,
+  TimeScale,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import "chartjs-adapter-moment"; // Required for time-based charts
+
+// ✅ Register required components
+ChartJS.register(LineElement, PointElement, LinearScale, TimeScale, Title, Tooltip, Legend);
 
 const options = {
-  legend: {
-    display: false,
-  },
-  elements: {
-    point: {
-      radius: 0,
-    },
-  },
+  responsive: true,
   maintainAspectRatio: false,
-  tooltips: {
-    mode: "index",
-    intersect: false,
-    callbacks: {
-      label: function (tooltipItem, data) {
-        return numeral(tooltipItem.value).format("+0,0");
+  plugins: {
+    legend: {
+      display: false,
+    },
+    tooltip: {
+      mode: "index",
+      intersect: false,
+      callbacks: {
+        label: function (tooltipItem) {
+          return numeral(tooltipItem.raw).format("+0,0");
+        },
       },
     },
   },
   scales: {
-    xAxes: [
-      {
-        type: "time",
-        time: {
-          format: "MM/DD/YY",
-          tooltipFormat: "ll",
+    x: {
+      type: "time",
+      time: {
+        unit: "day",
+        tooltipFormat: "ll",
+      },
+    },
+    y: {
+      ticks: {
+        callback: function (value) {
+          return numeral(value).format("0a");
         },
       },
-    ],
-    yAxes: [
-      {
-        gridLines: {
-          display: false,
-        },
-        ticks: {
-          // Include a dollar sign in the ticks
-          callback: function (value, index, values) {
-            return numeral(value).format("0a");
-          },
-        },
-      },
-    ],
+    },
   },
 };
 
@@ -71,20 +68,14 @@ const buildChartData = (data, casesType) => {
 };
 
 function LineGraph({ casesType }) {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          let chartData = buildChartData(data, casesType);
-          setData(chartData);
-          //   console.log(chartData);
-          //   buildChart(chartData);
-        });
+      const response = await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120");
+      const result = await response.json();
+      let chartData = buildChartData(result, casesType);
+      setData(chartData);
     };
 
     fetchData();
@@ -92,11 +83,12 @@ function LineGraph({ casesType }) {
 
   return (
     <div>
-      {data?.length > 0 && (
+      {data.length > 0 && (
         <Line
           data={{
             datasets: [
               {
+                label: "Cases",
                 backgroundColor: "rgba(204, 16, 52, 0.5)",
                 borderColor: "#CC1034",
                 data: data,
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./Map.css";
-import { Map as LeafletMap, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import { showDataOnMap } from "../util";
 
 function Map({ countries, center, zoom, casesType }) {
   return (
     <div className="map">
-      <LeafletMap center={center} zoom={zoom}>
+      <MapContainer center={center} zoom={zoom} style={{ height: "100%", width: "100%" }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         {showDataOnMap(countries, casesType)}
-      </LeafletMap>
+      </MapContainer>
     </div>
   );
 }
